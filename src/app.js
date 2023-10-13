79fa1708de0b327386990ee31f9030aac98ec857
vuhoangduc <93504335+vuhoangduc@users.noreply.github.com>
2023-10-13 09:19:27 +0700

    13/10/2023

--- src/app.js
+++ src/app.js
@@ -3,9 +3,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-res.send('Hello World!')
+    res.send('Hello World!')
+})
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+    console.log(`WSV eComerece start with ${port}`);
 })
