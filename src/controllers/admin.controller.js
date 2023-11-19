60c8233ce7ff4405f282b75793b828ed3f84bde3
ngkhacdai <ngkhacdai@gmail.com>
2023-11-19 22:28:57 +0700

    update admin service

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -6,7 +6,6 @@ class AdminController{
     getAllShopForAdmin = async(req,res,next)=>{
         new SuccessResponse({
             metadata: await AdminService.getAllShopForAdmin({
-                page:req.params.page
             })
         }).send(res);
         
@@ -14,35 +13,30 @@ class AdminController{
     getAllUserForAdmin = async(req,res,next) =>{
         new SuccessResponse({
             metadata: await AdminService.getAllUserForAdmin({
-                page:req.params.page
             })
         }).send(res);
     }
     getAllProductForAdmin = async (req, res, next) => {
         new SuccessResponse({
             metadata: await AdminService.getAllProductForAdmin({
-                page:req.params.page
             })
         }).send(res)
     }
     getAllCategoryForAdmin = async (req, res, next) => { 
         new SuccessResponse({
             metadata: await AdminService.getAllCategoryForAdmin({
-                page:req.params.page
             })
         }).send(res)
     }
     getAllOrderForAdmin = async (req, res, next) => { 
         new SuccessResponse({
             metadata: await AdminService.getAllOrderForAdmin({
-                page:req.params.page
             })
         }).send(res)
     }
     getAllDiscountForAdmin = async (req, res, next) => { 
         new SuccessResponse({
             metadata: await AdminService.getAllDiscountForAdmin({
-                page:req.params.page
             })
         }).send(res)
     }
