966bb7f2daeab700d10bcafd8cdde63fe27c4a59
ngkhacdai <ngkhacdai@gmail.com>
2023-11-19 18:57:56 +0700

    update, delete category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -10,6 +10,21 @@ class ProductController {
                 })
         }).send(res);
     }
+    updateCategory = async (req, res, next) => { 
+        new SuccessResponse({
+            metadata: await CategoryService.updateCategory(req.file.path,
+                {
+                    ...req.body
+                })
+        }).send(res);
+    }
+    deleteCategory = async (req, res, next) => { 
+        new SuccessResponse({
+            metadata: await CategoryService.deleteCategory({
+                id_category: req.body.id_category
+            })
+        }).send(res);
+    }
     getAllCategory = async (req, res, next) => {
         new SuccessResponse({
             message: 'Lấy tất cả loại sản phẩm thành công',
--- src/routes/categories/index.category.js
+++ src/routes/categories/index.category.js
@@ -6,7 +6,11 @@ const { authentication } = require('../../auth/authUtils');
 const upload = require('../../util/upload_file');
 router.use(authentication);
 
-router.post('/createCategory', upload.single('thumb') ,asyneHandler(CategoryController.createCategory));
+router.post('/createCategory', upload.single('thumb'), asyneHandler(CategoryController.createCategory));
+
+router.put('/updateCategory', upload.single('thumb'), asyneHandler(CategoryController.updateCategory));
+
+router.post('/deleteCategory', upload.single('thumb') ,asyneHandler(CategoryController.deleteCategory));
 
 router.get('/getAllCategory' , asyneHandler(CategoryController.getAllCategory) )
 
