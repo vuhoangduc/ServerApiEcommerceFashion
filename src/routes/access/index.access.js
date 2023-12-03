8b08a0276c6370c27ef3c62069877147d82ee03d
vuhoangduc <93504335+vuhoangduc@users.noreply.github.com>
2023-12-04 01:03:40 +0700

    xác thc otp khi đăng ký

--- src/routes/access/index.access.js
+++ src/routes/access/index.access.js
@@ -5,6 +5,7 @@ const { asyneHandler } = require('../../auth/checkAuth');
 const { authentication } = require('../../auth/authUtils');
 router.post('/signup',asyneHandler(accessController.signUp));
 router.post('/login',asyneHandler(accessController.logIn));
+router.post('/verifyOtp',asyneHandler(accessController.verifyOtp));
 router.use(authentication);
 router.delete('/signOut',asyneHandler(accessController.signOut));
 
