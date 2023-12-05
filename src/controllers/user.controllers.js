6d1843482a490267867a11e3b3d40e4aac487c08
ngkhacdai <ngkhacdai@gmail.com>
2023-12-05 09:46:46 +0700

    crud address

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -23,24 +23,47 @@ class UserController {
             })
         }).send(res);
     }
-    setUpAcc = async(req,res,next)=>{
-        if(!req.file){
-            req.file={
-                path:'1697261656746-profile.png'
+    setUpAcc = async (req, res, next) => {
+        if (!req.file) {
+            req.file = {
+                path: '1697261656746-profile.png'
             }
         }
         new SuccessResponse({
-            metadata: await UserService.updateUser(req.file.path,{
-                userId:req.params.id,
+            metadata: await UserService.updateUser(req.file.path, {
+                userId: req.params.id,
                 ...req.body
             })
         }).send(res);
     }
-
     getProfile = async (req, res, next) => {
         new SuccessResponse({
             metadata: await UserService.getProfile({
                 userId: req.user.userId,
+            })
+        }).send(res);
+    }
+    addAddress = async (req, res, next) => {
+        new SuccessResponse({
+            metadata: await UserService.addAddress({
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res);
+    }
+    deleteAddress = async (req, res, next) => {
+        new SuccessResponse({
+            metadata: await UserService.deleteAddress({
+                userId: req.user.userId,
+                ...req.body
+            })
+        }).send(res);
+    }
+    updateAddress = async (req, res, next) => {
+        new SuccessResponse({
+            metadata: await UserService.updateAddress({
+                userId: req.user.userId,
+                ...req.body
             })
         }).send(res);
     }
